a5052aa9004d98f5eca7c5675552f358fc5954a6
James Hugman <james@hugman.tv>
2021-08-30 17:26:19 +0100

    Flesh out fixture for delegates in Kotlin

--- fixtures/delegates/src/lib.rs
+++ fixtures/delegates/src/lib.rs
@@ -1,21 +1,33 @@
+use std::convert::TryInto;
+
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #[derive(Debug, Clone)]
-pub struct RustObject;
+pub struct RustObject {
+    string: String,
+}
 
 impl RustObject {
     fn new() -> Self {
-        Self
+        Self { string: "".into() }
     }
 
-    fn from_string(_s: String) -> Self {
-        Self
+    fn from_string(string: String) -> Self {
+        Self { string }
     }
 
     fn identity_string(&self, s: String) -> String {
         s
+    }
+
+    fn get_string(&self) -> String {
+        self.string.clone()
+    }
+
+    fn length(&self) -> i32 {
+        self.string.len().try_into().unwrap()
     }
 }
 
--- fixtures/delegates/tests/bindings/test_delegates.kts
+++ fixtures/delegates/tests/bindings/test_delegates.kts
@@ -4,7 +4,35 @@
 
 import uniffi.delegates.*
 
-object Delegate : MyDelegate {
-    override fun <T> withReturn(thunk: () -> T): T = thunk().let { it }
-    override fun <T> withoutReturn(thunk: () -> T) = thunk().let { Unit }
+/// MyDelegate is defined in the UDL file. It generates an interface.
+/// Implementations of the interface never cross the FFI boundary, and so
+/// can contain arbitrary Kotlin.
+class Delegate : MyDelegate {
+    var count = 0
+    var lastString: String? = null
+
+    override fun <T> withReturn(thunk: () -> T): T = thunk()
+    override fun <T> stringSaver(thunk: () -> T) {
+        lastString = thunk() as? String
+    }
+    override fun <T> withCounter(thunk: () -> T): Int = thunk().let { ++count }
 }
+
+val delegate = Delegate()
+// Delegates are given to the rust object via a constructor.
+val rustObj0 = RustObject(delegate)
+
+val string0 = "placeholder string"
+// Alternative constructors take the delegate as the first argument.
+// The argument name is a mixed case version of the delegate interface name.
+val rustObj1 = RustObject.fromString(string = string0, myDelegate = delegate)
+
+assert(rustObj0.length() == string0.length) { "generic return" }
+
+assert(rustObj0.getString() == 1) { "different return type from method's own" }
+assert(rustObj0.getString() == 2) { "code is run each time the method is run" }
+assert(rustObj1.getString() == 3) { "delegate can be shared between objects" }
+
+val string1 = "meta-syntactic variable values"
+assert(rustObj1.identityString(string1) == Unit) { "void return" }
+assert(delegate.lastString == string1) { "" }
